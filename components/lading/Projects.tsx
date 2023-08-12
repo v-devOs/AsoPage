4cb46ba26c04901bd58a3ee02417df10c895fdfc
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-08-12 10:17:44 -0600

    Pagina de proyectos terminada

--- components/lading/Projects.tsx
+++ components/lading/Projects.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
-import { Button, Grid, Typography } from '@mui/material'
+import { Box, Grid, Typography } from '@mui/material'
 import ArrowDown from '@mui/icons-material/KeyboardDoubleArrowDownOutlined';
+import ArrowUp from '@mui/icons-material/KeyboardDoubleArrowUpOutlined';
 
 import { DecoredTitle } from '../ui'
 import { projectsAso } from '@/data'
 
 export const Projects = () => {
 
-  const [nameProjectToShow, setNameProjectToShow] = useState('')
+  const [nameProjectToShow, setNameProjectToShow] = useState('Aplicación para cafetería')
 
   const onClick = ( name: string ) => {
     const valueToSet = name === nameProjectToShow ? '' : name
@@ -26,35 +27,42 @@ export const Projects = () => {
       </Grid>
 
       <Grid item xs={12} md={6}>
+        <Grid container>
           {
             projectsAso.map( ({ _id, desc, nameProject, membersManager }) => (
-              <Button
-                className='' 
-                key={ _id} 
-                fullWidth
-                sx={{ mb: 1, background: '#E7e7e7', color: '#101010', display: 'flex', flexWrap: 'wrap'}}
-                variant='contained'
-                onClick={ () => onClick( nameProject )}
-              >
-                <Typography variant='body1'>{ nameProject } { <ArrowDown/>}</Typography>
 
-                <Grid
-                  container
-                  sx={{ 
+              <Grid
+                onClick={() => onClick(nameProject)} 
+                item
+                key={_id}
+                xs={12}
+                sx={{background: '#E7e7e7', color: '#101010', mb: 1, p: 2, borderRadius: '10px'}}
+              >
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+                  <Typography variant='body1'>{ nameProject  }</Typography>
+
+                  <ArrowDown
+                    className='fade-in' 
+                    sx={{ display: nameProject === nameProjectToShow ? 'none' : 'bock'}}/>
+                  <ArrowUp
+                    className='fade-in' 
+                    sx={{ display: nameProject === nameProjectToShow ? 'block' : 'none'}}/>
+                </Box>
+
+
+                <Grid className='fade-in' sx={{ 
                     background: 'whitesmoke', 
-                    p: 2, display: nameProject === nameProjectToShow ? 'block' : 'none', 
-                    borderRadius: '5px',
-                    textAlign: 'justify'
+                    p: 2, borderRadius: '5px', 
+                    display: nameProject === nameProjectToShow ? 'block' : 'none'
                   }}
-                  className='fade-in'
                 >
-                  <Typography variant='body2'>
-                    { desc }
-                  </Typography>
+                  <Typography variant='body2'>{ desc }</Typography>
                 </Grid>
-              </Button>
+
+              </Grid>
             ))
           }
+        </Grid>
       </Grid>
 
     </Grid>
