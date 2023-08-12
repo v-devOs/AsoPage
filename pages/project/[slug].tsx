396922ccbff0f03cdf73db3039565bf0383d92e6
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-08-12 10:02:31 -0600

    Agregando mini-contenido a la pagina del projecto

---
+++ pages/project/[slug].tsx
@@ -0,0 +1,10 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+const ProjectPage = () => {
+  return (
+    <Typography>Proxiamente</Typography>
+  )
+}
+
+export default ProjectPage
