977fc426cfdec5bc6d54a1d17730b8aa0799bcb2
Luis Leal <luisrrleal@gmail.com>
2023-08-12 19:13:21 -0600

    Change home

--- components/lading/Proposals.tsx
+++ components/lading/Proposals.tsx
@@ -5,30 +5,30 @@ import { proposalsAso } from '../../data/proposalsAso';
 
 export const Proposals = () => {
   return (
-    <Grid container sx={{ background: '#101010', p: 2}} spacing={4}>
+    <Grid container sx={{ background: '#101010', p: 2 }} spacing={4}>
 
-      <Grid item  xs={12} md={6} sx={{ color: 'white', display: { xs: 'block', md: 'none'}}}>
+      <Grid item xs={12} md={6} sx={{ color: 'white', display: { xs: 'block', md: 'none' } }}>
         <DecoredTitle title='Propuestas'>
-            <Typography variant='body1'>
-              Elit sunt deserunt duis esse tempor ea. Consequat ullamco elit esse ut exercitation sit ullamco ea non deserunt aliqua dolore aliquip dolore. Ut velit Lorem enim laboris velit cupidatat tempor. Sint culpa officia culpa eu commodo magna anim aute. Sunt fugiat reprehenderit labore veniam ad est est enim. Id aliqua consectetur officia nisi commodo laboris quis tempor incididunt reprehenderit pariatur amet ullamco ad.
-            </Typography>
+          <Typography variant='body1'>
+            Elit sunt deserunt duis esse tempor ea. Consequat ullamco elit esse ut exercitation sit ullamco ea non deserunt aliqua dolore aliquip dolore. Ut velit Lorem enim laboris velit cupidatat tempor. Sint culpa officia culpa eu commodo magna anim aute. Sunt fugiat reprehenderit labore veniam ad est est enim. Id aliqua consectetur officia nisi commodo laboris quis tempor incididunt reprehenderit pariatur amet ullamco ad.
+          </Typography>
         </DecoredTitle>
       </Grid>
-      
+
 
       <Grid item xs={12} md={6}>
         <Grid container spacing={2}>
 
           {
-            proposalsAso.map( p => (
+            proposalsAso.map(p => (
               <Grid item xs={12} md={6} key={p._id}>
                 <Card className='appearAndIncrementWidth'>
                   <CardContent>
-                    <Typography variant='body1'>{ p.nameProposal}</Typography>
-                    <Divider/>
-                    <Typography variant='body2'>{ p.desc }</Typography>
+                    <Typography variant='body1'>{p.nameProposal}</Typography>
+                    <Divider />
+                    <Typography variant='body2'>{p.desc}</Typography>
                   </CardContent>
-                  
+
                 </Card>
               </Grid>
             ))
@@ -37,14 +37,13 @@ export const Proposals = () => {
         </Grid>
       </Grid>
 
-      <Grid item xs={12} md={6} sx={{ color: 'white', display: { xs: 'none', md: 'block'}}}>
+      <Grid item xs={12} md={6} sx={{ color: 'white', display: { xs: 'none', md: 'block' } }}>
         <DecoredTitle title='Propuestas'>
-            <Typography variant='body1'>
-              Elit sunt deserunt duis esse tempor ea. Consequat ullamco elit esse ut exercitation sit ullamco ea non deserunt aliqua dolore aliquip dolore. Ut velit Lorem enim laboris velit cupidatat tempor. Sint culpa officia culpa eu commodo magna anim aute. Sunt fugiat reprehenderit labore veniam ad est est enim. Id aliqua consectetur officia nisi commodo laboris quis tempor incididunt reprehenderit pariatur amet ullamco ad.
-            </Typography>
+          <Typography variant='body1'>
+            Elit sunt deserunt duis esse tempor ea. Consequat ullamco elit esse ut exercitation sit ullamco ea non deserunt aliqua dolore aliquip dolore. Ut velit Lorem enim laboris velit cupidatat tempor. Sint culpa officia culpa eu commodo magna anim aute. Sunt fugiat reprehenderit labore veniam ad est est enim. Id aliqua consectetur officia nisi commodo laboris quis tempor incididunt reprehenderit pariatur amet ullamco ad.
+          </Typography>
         </DecoredTitle>
       </Grid>
-
     </Grid>
   )
 }
